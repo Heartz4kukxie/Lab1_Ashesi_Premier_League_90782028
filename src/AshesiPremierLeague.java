ea2590c1a5fa57bbdcf035039c3a8f5d8cc0dee6
Heartz4kukxie <kuukuaeshun151@gmail.com>
2025-09-22 14:44:07 +0000

    For Task one,I declared variables for the player details.That is  name, age, height (in meters), weight (in pounds),and jersey number.

--- src/AshesiPremierLeague.java
+++ src/AshesiPremierLeague.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class AshesiPremierLeague {
     public static void main(String[] args) {
@@ -8,6 +9,40 @@ public class AshesiPremierLeague {
         int jersey_number;
         double weight;
         double height;
+
+
+        //Accepting input from the user
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of the player");
+        name = input.nextLine();
+
+        System.out.println("Enter the age of the player");
+        age = input.nextInt();
+
+        System.out.println("Enter the height of the player");
+        height = input.nextDouble();
+
+        System.out.println("Enter the weight  of the player");
+        weight = input.nextDouble();
+
+        System.out.println("Enter the jersey number of the player");
+         jersey_number = input.nextInt();
+
+         //Code for output of  the details
+
+        System.out.println("Player Name - " + name);
+        System.out.println("Age - " + age);
+        System.out.println("Height -" + height);
+        System.out.println("Weight -" + weight);
+        System.out.println("Jersey number -" + jersey_number);
+
+
+
+        //TASK 2
+        // Declaring constat variables to convert weight to kilograms and the height to centimeters
+        final double pound =0.45359237;
+
+
 
     }
 }
