5dd06ce0c14609075afdf80f5339135baba4d71d
Heartz4kukxie <kuukuaeshun151@gmail.com>
2025-09-22 16:02:01 +0000

    For Task one,I declared variables for the player details.That is  name, age, height (in meters), weight (in pounds),and jersey number.

--- src/AshesiPremierLeague.java
+++ src/AshesiPremierLeague.java
@@ -19,10 +19,10 @@ public class AshesiPremierLeague {
         System.out.println("Enter the age of the player");
         age = input.nextInt();
 
-        System.out.println("Enter the height of the player");
+        System.out.println("Enter the height of the player(in meters)");
         height = input.nextDouble();
 
-        System.out.println("Enter the weight  of the player");
+        System.out.println("Enter the weight  of the player(in pounds)");
         weight = input.nextDouble();
 
         System.out.println("Enter the jersey number of the player");
@@ -40,7 +40,84 @@ public class AshesiPremierLeague {
 
         //TASK 2
         // Declaring constat variables to convert weight to kilograms and the height to centimeters
-        final double pound =0.45359237;
+        final double POUND =0.45359237;
+        final int METER = 100;
+
+        // converting weight to kg and height to centimeters
+
+         double convertingWeight = weight * POUND;
+         double convertingHeight = height *  METER;
+
+        // Round weight to an integer using casting
+         int weightInKg = (int)convertingWeight;
+         int heightInCentimeters  = (int)convertingHeight;
+
+        //Code for output of  the details after conversion
+
+        System.out.println("Player Name - " + name);
+        System.out.println("Age - " + age);
+        System.out.println("Height in centimeters  -" + heightInCentimeters + "cm");
+        System.out.println("Weight  in Kilogram-" + weightInKg + "kg");
+        System.out.println("Jersey number -" + jersey_number);
+
+
+
+        //Task 3 Increment and Decrement
+
+        //Task 4
+        //Checking eligibility of player
+
+        if (age >= 18 &&  age <= 35 && weight <90){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Not Eligible");
+        }
+
+        //Task 5
+        //Categorizing players by age
+        if(age < 20){
+            System.out.println("Rising Star");
+        }else if(age >= 20 && age < 30){
+            System.out.println("Prime Player");
+        } else if (age >30){
+            System.out.println("Veteran");
+        }
+        switch (jersey_number){
+            case 1:
+                System.out.println("Goalkeeper" );
+                break;
+            case 2:
+            case 5:
+                System.out.println("Defender");
+                break;
+
+            case 6:
+            case 8:
+                System.out.println("Midfielder,");
+                break;
+            case 7:
+            case 11:
+
+                System.out.println("Winger");
+                break;
+
+            case 9:
+                System.out.println("Striker");
+                break;
+
+            case 10:
+                System.out.println("Playmaker");
+                break;
+            default:
+                System.out.println("Player position not known");
+
+
+
+        }
+
+
+
+
 
 
 
