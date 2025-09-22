89d8684e8e2f483fa8055f13e90effb79c440894
Heartz4kukxie <kuukuaeshun151@gmail.com>
2025-09-22 14:22:32 +0000

    For Task one,I declared variables for the player details.That is  name, age, height (in meters), weight (in pounds),and jersey number.

--- src/AshesiPremierLeague.java
+++ src/AshesiPremierLeague.java
@@ -1,6 +1,8 @@
 
 public class AshesiPremierLeague {
     public static void main(String[] args) {
+
+        // declaring variables
         String name;
         int age;
         int jersey_number;
